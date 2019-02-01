3ce080d98c9cec94cc4ed4f964514c0ea37a3270
Robin Tram <robin.tram@gmail.com>
2019-02-01 12:18:39 -0500

    Convert SignIn Page into Functional Component
    Converted SignIn Page from Class Component to Functional Component to reduce load times.

--- src/SignInView/SignIn.js
+++ src/SignInView/SignIn.js
@@ -1,32 +1,31 @@
-import React, { Component } from "react"
+import React from "react"
 import {Button} from "react-bootstrap"
 import {Link} from 'react-router-dom'
 
-export default class Footer extends Component {
+const SignIn = () => (
+  <div>
+    Sign In
+    <br/>
+    <br/>
+    <Link to={`/users/1/`}>
+      <Button bsStyle="primary" block><strong>Robin Tram</strong></Button>
+    </Link>
 
-  render() {
+    <Link to={`/users/2/`}>
+      <Button bsStyle="primary" block><strong>Kevin Tram</strong></Button>
+    </Link>
 
-    return (
-      <div>
-        Sign In
-        <br/>
-        <br/>
-        <Link to={`/users/1/`}>
-          <Button bsStyle="primary" block><strong>Robin Tram</strong></Button>
-        </Link>
+    <Link to={`/users/3/`}>
+      <Button bsStyle="primary" block><strong>Sarah Tram</strong></Button>
+    </Link>
 
-        <Link to={`/users/2/`}>
-          <Button bsStyle="primary" block><strong>Kevin Tram</strong></Button>
-        </Link>
+    <br/>
+    <br/>
+    <br/>
+  </div>
+)
 
-        <Link to={`/users/3/`}>
-          <Button bsStyle="primary" block><strong>Sarah Tram</strong></Button>
-        </Link>
 
-        <br/>
-        <br/>
-        <br/>
-      </div>
-    )
-  }
-}
+
+
+export default SignIn
